30d5bb79d5c626f706e6998de7e28ff05438b070
Fazil Basri <fazillbasry@gmail.com>
2022-04-15 02:56:38 +0700

    auth register work

--- app/src/main/java/com/uas/halolaw/Sign.kt
+++ app/src/main/java/com/uas/halolaw/Sign.kt
@@ -1,11 +1,19 @@
 package com.uas.halolaw
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign.*
 
 class Sign : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign)
+
+        btnRegister.setOnClickListener{
+            Intent(this@Sign, Register::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
