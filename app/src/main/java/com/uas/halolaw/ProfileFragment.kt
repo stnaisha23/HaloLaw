efb8f07b90d87a4defeffe12f3e3f4832238c8dc
Fazil Basri <fazillbasry@gmail.com>
2022-04-17 03:35:05 +0700

    button fragment info app

--- app/src/main/java/com/uas/halolaw/ProfileFragment.kt
+++ app/src/main/java/com/uas/halolaw/ProfileFragment.kt
@@ -1,19 +1,18 @@
 package com.uas.halolaw
 
-import android.annotation.SuppressLint
 import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
-import android.opengl.Visibility
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.UserProfileChangeRequest
@@ -22,6 +21,7 @@ import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_sign.*
 import kotlinx.android.synthetic.main.fragment_profile.*
 import java.io.ByteArrayOutputStream
+
 
 @Suppress("DEPRECATION")
 class ProfileFragment : Fragment() {
@@ -71,8 +71,10 @@ class ProfileFragment : Fragment() {
 
         //button info App
         btnInfoApp.setOnClickListener{
-            
+            val myIntent = Intent(activity, InfoApp::class.java)
+            startActivity(myIntent)
         }
+
 
         //btn edit profile
         btnEdit.setOnClickListener{
