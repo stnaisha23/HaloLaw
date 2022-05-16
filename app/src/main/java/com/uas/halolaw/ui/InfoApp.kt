1731991fb75430314c354188029545c3d0461a6b
Fazil Basri <fazillbasry@gmail.com>
2022-05-16 15:43:26 +0700

    update

--- app/src/main/java/com/uas/halolaw/ui/InfoApp.kt
+++ app/src/main/java/com/uas/halolaw/ui/InfoApp.kt
@@ -1,14 +1,21 @@
 package com.uas.halolaw.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.uas.halolaw.R
+import kotlinx.android.synthetic.main.activity_info_app.*
+import kotlinx.android.synthetic.main.activity_landing_page.*
 
 class InfoApp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info_app)
 
-
+        btnLawbot.setOnClickListener{
+            Intent(this@InfoApp, MainLawbot::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
